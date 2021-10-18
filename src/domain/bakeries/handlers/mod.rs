5dfaaa1073db12752ed81c50ba5ac5fbf23f91e2
Sam Samai <sam@studio2pi.com.au>
2021-10-19 08:41:37 +1100

    Separate Input Model

--- src/domain/bakeries/handlers/mod.rs
+++ src/domain/bakeries/handlers/mod.rs
@@ -8,7 +8,7 @@ use super::bakery;
 use crate::db::pool;
 
 pub fn routes() -> Vec<rocket::Route> {
-    routes![all, create]
+    routes![all, create, get]
 }
 
 #[get("/")]
@@ -26,19 +26,88 @@ pub async fn all(
     // .map_err(|error| error_status(error))
 }
 
+#[get("/<id>")]
+pub async fn get(
+    connection: Connection<pool::Db>, id: i32
+) -> Result<Json<Option<super::bakery::Model>>, Status> {
+println!("handler id: {:#?}", id);
+let v =         Bakery::find()
+            .all(&connection)
+            .await
+            .expect("could not retrieve bakeries")
+            .into_iter()
+            .collect::<Vec<_>>();
+
+           println!("v: {:#?}", v);
+
+
+    let bakery =         Bakery::find_by_id(id)
+            .one(&connection)
+            .await
+            .unwrap();
+println!("handler bakery: {:#?}", bakery);
+
+    Ok(Json(bakery ))
+}
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Clone, Debug, PartialEq, serde::Serialize, Deserialize)]
+pub struct BakeryInput {
+    pub name: String,
+    pub profit_margin: f64,
+}
+
+
 #[post("/", data = "<new_bakery>")]
-async fn create(conn: Connection<pool::Db>, new_bakery: Json<bakery::Model>) -> Result<Json<super::bakery::Model>, Status> {
+async fn create(conn: Connection<pool::Db>, new_bakery: Json<BakeryInput>) -> Result<Json<super::bakery::Model>, Status> {
     let mut new_bakery = new_bakery.clone().into_inner();
 
     let bakery = bakery::ActiveModel {
         name: Set(new_bakery.name.to_owned()),
         profit_margin: Set(new_bakery.profit_margin),
         ..Default::default()
-    }
-    .save(&conn)
-    .await
-    .expect("error");
-    new_bakery.id = bakery.id.unwrap();
+    };
 
-    Ok(Json(new_bakery))
+        let res = Bakery::insert(bakery)
+        .exec(&conn)
+        .await
+        .expect("could not insert bakery");
+
+
+    // .save(&conn)
+    // .await
+    // .expect("error");
+    println!("!! create bakery: {:#?}", res);
+
+        let new_bakery: bakery::Model = Bakery::find_by_id(res.last_insert_id)
+        .one(&conn)
+        .await
+        .expect("could not find bakery")
+        .unwrap();
+
+    // new_bakery.id = bakery.id.unwrap();
+println!("new_bakery: {:#?}", new_bakery);
+
+let result = Json(new_bakery);
+println!("@@ json result: {:#?}", result);
+
+    Ok(result)
 }
+
+
+    // let baker_bob = baker::ActiveModel {
+    //     name: Set("Baker Bob".to_owned()),
+    //     contact_details: Set(serde_json::json!(baker_bob_contact)),
+    //     bakery_id: Set(Some(bakery_insert_res.last_insert_id as i32)),
+    //     ..Default::default()
+    // };
+    // let res = Baker::insert(baker_bob)
+    //     .exec(db)
+    //     .await
+    //     .expect("could not insert baker");
+
+    // let baker: Option<baker::Model> = Baker::find_by_id(res.last_insert_id)
+    //     .one(db)
+    //     .await
+    //     .expect("could not find baker");
--- tests/bakeries_test.rs
+++ tests/bakeries_test.rs
@@ -7,23 +7,58 @@ use rocket::local::asynchronous::{Client, LocalResponse};
 
 use sea_orm_rocket_starter::domain::bakeries::*;
 
+// #[rocket::async_test]
+// async fn all() {
+
+//     rocket::tokio::task::spawn(async move {
+//         let test_context = TestContext::init().await;
+//         // println!("@@ all() test_context: {:#?}", test_context.client);
+
+//         create_bakery(&test_context.client).await;
+//         create_bakery(&test_context.client).await;
+//         let response = test_context.client
+//             .get("/bakeries")
+//             .header(ContentType::JSON)
+//             .dispatch().await.into_json::<Vec<bakery::Model>>().await;
+
+//         let bakery_vec = response.expect("no bakeries returned!");
+//         assert_eq!(bakery_vec.len(), 2);
+
+//         TestContext::tear_down(&test_context).await;
+//     }).await.expect("Task panicked");
+
+// }
+
 #[rocket::async_test]
-async fn all() {
-    let test_context = TestContext::init().await;
-    create_bakery(&test_context.client).await;
-    create_bakery(&test_context.client).await;
-    let response = test_context.client
-        .get("/bakeries")
-        .header(ContentType::JSON)
-        .dispatch().await.into_json::<Vec<bakery::Model>>().await;
+async fn get() {
+    rocket::tokio::task::spawn(async move {
+        let test_context = TestContext::init().await;
+            // println!("@@ get() test_context: {:#?}", test_context.client);
 
-    let bakery_vec = response.expect("no bakeries returned!");
-    assert_eq!(bakery_vec.len(), 2);
+        let r = create_bakery(&test_context.client).await;
+        println!("r: {:#?}", r);
 
-    TestContext::tear_down(&test_context).await;
+        let response = test_context.client
+            .get(format!("/bakeries/{}", r.id))
+            .header(ContentType::JSON)
+            .dispatch()
+            .await;
+            println!("response: {:#?}", response);
+
+        // let r_bakery = response
+        //     .into_json::<bakery::Model>()
+        //     .await
+        //     .unwrap();
+    println!("response: {:#?}", response);
+
+        // assert_eq!(r_bakery.name, "Test Bakery");
+
+        TestContext::tear_down(&test_context).await;
+    }).await.expect("Task panicked");
+
 }
 
-async fn create_bakery(client: &Client) -> LocalResponse<'_> {
+async fn create_bakery(client: &Client) -> bakery::Model {
     let response = client
         .post("/bakeries")
         .header(ContentType::JSON)
@@ -36,6 +71,9 @@ async fn create_bakery(client: &Client) -> LocalResponse<'_> {
         .dispatch().await;
 
     assert_eq!(response.status(), Status::Ok);
+    let r = response.into_json::<bakery::Model>().await.expect("bakery");
+    println!("create_bakery response: {:#?}", r);
 
-    response
+    r
+
 }
