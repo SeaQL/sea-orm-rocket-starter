d2f892e0edd41c980e5fc637e9d55caf4b55c073
Sam Samai <sam@studio2pi.com.au>
2021-10-20 22:23:17 +1100

    Bakery delete

--- tests/bakeries_test.rs
+++ tests/bakeries_test.rs
@@ -12,6 +12,7 @@ async fn main() {
     all().await;
     get().await;
     update().await;
+    delete().await;
 }
 
 async fn all() {
@@ -76,6 +77,36 @@ async fn update() {
 
     assert_eq!(r_bakery.name, "Updated Bakery");
     assert_eq!(r_bakery.profit_margin, 20.1);
+
+    TestContext::tear_down(&test_context).await;
+}
+
+async fn delete() {
+    let test_context = TestContext::init("bakery_delete").await;
+
+    let r = create_bakery(&test_context.client).await;
+
+    let response = test_context.client
+        .get("/bakeries")
+        .header(ContentType::JSON)
+        .dispatch().await.into_json::<Vec<bakery::Model>>().await;
+
+    let bakery_vec = response.expect("no bakeries returned!");
+    assert_eq!(bakery_vec.len(), 1);
+
+    let response = test_context.client
+        .delete(format!("/bakeries/{}", r.id))
+        .header(ContentType::JSON)
+        .dispatch().await;
+
+    assert_eq!(response.status(), Status::Ok);
+    let response = test_context.client
+        .get("/bakeries")
+        .header(ContentType::JSON)
+        .dispatch().await.into_json::<Vec<bakery::Model>>().await;
+
+    let bakery_vec = response.expect("no bakeries returned!");
+    assert_eq!(bakery_vec.len(), 0);
 
     TestContext::tear_down(&test_context).await;
 }
